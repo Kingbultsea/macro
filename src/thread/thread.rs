9ca5b5224ac5ea0d88db3ac38ff68dd3eb38208d
kingbultsea <894275157@qq.com>
2023-05-09 16:07:22 +0800

    😯

---
+++ src/thread/thread.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // 创建一个线程A
+    let new_thread = thread::spawn(move || {
+        // 再创建一个线程B
+        thread::spawn(move || {
+            loop {
+                println!("I am a new thread.");
+            }
+        })
+    });
+
+    // 等待新创建的线程执行完成
+    new_thread.join().unwrap();
+    println!("Child thread is finish!");
+
+    // 睡眠一段时间，看子线程创建的子线程是否还在运行
+    thread::sleep(Duration::from_millis(50));
+
+    // 结论 B在A结束后依旧是会运行的，只要main线程不结束
+}
